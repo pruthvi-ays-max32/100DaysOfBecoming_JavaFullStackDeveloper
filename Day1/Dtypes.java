6c8c76923cbeff7d534a3ee2e5ec9c6df0278902
pruthvi-ays-max32 <156766960+pruthvi-ays-max32@users.noreply.github.com>
2025-09-28 12:16:38 +0000

    day 1 : intro to java & dtypes in java

---
+++ Day1/Dtypes.java
@@ -0,0 +1,9 @@
+class Dtypes {
+    public static void main(String args[]){
+        int age = 23;
+        double salary = 55000.5;
+        boolean isLearning = true;
+        String name = "Pruthaviraj";
+        System.out.printf("Name: %s, Age: %d, Salary: %.1f, Learning Full Stack: %b", name, age, salary, isLearning);
+    }
+}
